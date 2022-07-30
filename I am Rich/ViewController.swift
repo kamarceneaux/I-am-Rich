9cce72e13a54efc9c47e598b717fb964c0315f90
Kameron Arceneaux <kameronarceneaux05@gmail.com>
2022-07-29 19:55:06 -0500

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Kameron Arceneaux on 7/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
